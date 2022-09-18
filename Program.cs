89136400a8d106745bac6e1e347740a960751224
islam <mamaev408@gmail.com>
2022-09-18 13:30:35 +0500

    введение значений

---
+++ Program.cs
@@ -0,0 +1,7 @@
+Console.Write("Задайте количество строк: ");
+int length = Covert.ToInt32(Console.Raedline());
+string[] array = new string[length];
+Console.Write("Задайте значение строк: ");
+int counter = 0;
+
+
