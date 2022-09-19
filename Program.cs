35c39cb0fc44c3b1ef734d53496ce39c4e489410
islam <mamaev408@gmail.com>
2022-09-19 14:17:34 +0500

    часть кода

--- Program.cs
+++ Program.cs
@@ -4,4 +4,22 @@ string[] array = new string[length];
 Console.Write("Задайте значение строк: ");
 int counter = 0;
 
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    array[i] = Covert.ToInt32(Console.Raedline());
+    if(array[i].Length<= 3) counter++; 
+}
+string[] secondArray = new string [counter];
+counter = 0;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    if (array[i].Length <= 3)
+    {
+        secondArray[counter] = array[i];
+        counter++;
+    }
+}
+PrintArray(array);
+Console.Write(" - ");
+PrintArray(secondArray);
 
